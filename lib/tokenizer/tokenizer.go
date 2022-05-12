a0a7f94f5d917d0c9428c33961d91453f6b5b8b1
eli <elilevin11@gmail.com>
2022-05-12 20:30:10 +0300

    skeleton

---
+++ lib/tokenizer/tokenizer.go
@@ -0,0 +1,37 @@
+package tokenizer
+
+func tokenize(path string) {
+
+}
+
+func hasMoreTokens() bool {
+
+}
+
+func advance() {
+
+}
+
+func tokenType() string {
+
+}
+
+func keyword() string {
+
+}
+
+func symbol() byte {
+
+}
+
+func identifier() string {
+
+}
+
+func intVal() int {
+
+}
+
+func stringVal() string {
+
+}
