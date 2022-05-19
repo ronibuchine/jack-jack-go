6018b5302b88881f6e446ebcaa47c0691b00ecf2
ronibuchine <ronibuchine@gmail.com>
2022-05-19 20:53:44 +0300

    worked on some grammar functions

--- lib/compiler/syntaxanalyzer.go
+++ lib/compiler/syntaxanalyzer.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 )
 
 // aliasing structs for xml objects
@@ -26,6 +27,10 @@ type Node struct {
 	children []Node
 }
 
+// globals for matching
+var normalizedTokenStream [][]string
+var current int = 0
+
 // Takes an ordered token stream which is a map[string]string and parses it and returns the XML tree
 func BuildXML(tokenStream map[string]string) *os.File {
 	output, err := os.Create("output.xml")
@@ -38,8 +43,8 @@ func BuildXML(tokenStream map[string]string) *os.File {
 	return output
 }
 
-// Reads in the token stream form the XML file, returns as a list of tokens
-func ReadStream(tokenStream string) [][]string {
+// Reads in the token stream form the XML file, returns as a list of tokens to the global token stream
+func ReadStream(tokenStream string) {
 	input, err := os.Open(tokenStream)
 	if err != nil {
 		log.Fatal(err)
@@ -57,29 +62,128 @@ func ReadStream(tokenStream string) [][]string {
 		fmt.Println("Type: " + token.Type)
 		fmt.Println("XMLName: " + token.XMLName.Local)
 	}
-	return getTokenStrings(tokens.Tokens)
+	getTokenStrings(tokens.Tokens)
 }
 
 // returns an array of tuples, first value in the tuple is the token, second value is the token type
-func getTokenStrings(tokens []Token) [][]string {
-	var tokenStrings [][]string
+func getTokenStrings(tokens []Token) {
 	for i := 0; i < len(tokens); i++ {
-		tokenStrings[i][0] = tokens[i].Token
-		tokenStrings[i][1] = tokens[i].Type
+		normalizedTokenStream[i][0] = tokens[i].Token
+		normalizedTokenStream[i][1] = tokens[i].Type
 	}
-	return tokenStrings
 }
 
 // global variable used for token parsing and matching
-var current int = 0
-
-func match(tokens []Token, token string) Node {
-	if current >= len(tokens) {
+func match(token string) Node {
+	if current >= len(normalizedTokenStream) {
 		log.Fatal("end of token stream")
 	}
-	if token == tokens[current].XMLName.Local {
+	if token == normalizedTokenStream[current][0] {
+		current++
 		return Node{token, []Node{}}
 	} else {
 		return Node{"", []Node{}}
+	}
+}
+
+func (parent Node) addChild(child Node) {
+	parent.children = append(parent.children, child)
+}
+
+// functions for grammar
+func letStatement() {
+
+}
+
+func whileStatement() {
+
+}
+
+func ifStatement() {
+
+}
+
+func doStatement() {
+
+}
+
+func statement() {
+
+}
+
+func statements() {
+
+}
+
+func expression() {
+
+}
+
+func term() {
+
+}
+
+func varName() {
+
+}
+
+func constant() Node {
+	result := Node{"constant", []Node{}}
+	value, _ := strconv.Atoi(normalizedTokenStream[current][0])
+	if value >= 0 || value < 32000 {
+		result.addChild(match(normalizedTokenStream[current][0]))
+	} else {
+		// TODO: error
+	}
+	return result
+}
+
+func op() Node {
+	result := Node{"op", []Node{}}
+	if normalizedTokenStream[current][0] == "+" {
+		result.addChild(match("+"))
+	} else if normalizedTokenStream[current][0] == "-" {
+		result.addChild(match("-"))
+	} else if normalizedTokenStream[current][0] == "=" {
+		result.addChild(match("="))
+	} else {
+		// TODO:  error logging
+	}
+	return result
+}
+
+func relop() {
+	if normalizedTokenStream[current][0] == "<" {
+
+	} else if normalizedTokenStream[current][0] == ">" {
+
+	} else {
+		// TODO: error logging
+	}
+}
+
+func boolOp() {
+	if normalizedTokenStream[current][0] == "&" {
+
+	} else if normalizedTokenStream[current][0] == "|" {
+
+	} else {
+		// TODO: error logging
+	}
+}
+
+func unaryOp() {
+	if normalizedTokenStream[current][0] == "-" {
+
+	} else {
+		// TODO: error logging
+	}
+}
+
+func boolUnaryOp() {
+	if normalizedTokenStream[current][0] == "~" {
+
+	} else {
+		// TODO: error logging
 	}
 }
