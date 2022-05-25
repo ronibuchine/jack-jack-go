340716d740c4c709e7f334f0fbbba53ab78a98dd
ronibuchine <ronibuchine@gmail.com>
2022-05-25 16:58:09 +0300

    pushed new test for errors

--- lib/syntaxAnalyzer/syntaxanalyzer_test.go
+++ lib/syntaxAnalyzer/syntaxanalyzer_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestParse(t *testing.T) {
 	var b bytes.Buffer
-	BuildXML(Parse(Tokenize("test.jack")), &b)
+	BuildXML(Parse(Tokenize("test2.jack")), &b)
 	fmt.Print(b.String())
 }
 
 func TestTokenize(t *testing.T) {
 	var b bytes.Buffer
-	TokenToXML(Tokenize("test.jack"), &b)
+	TokenToXML(Tokenize("test2.jack"), &b)
 	fmt.Print(b.String())
 }
