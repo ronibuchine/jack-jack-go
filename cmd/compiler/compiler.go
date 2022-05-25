ec7ada180df23fa1fcd9a3219b0eb68ab0e4f181
eli <elilevin11@gmail.com>
2022-05-26 00:13:59 +0300

    boom. Multithreading

--- cmd/compiler/compiler.go
+++ cmd/compiler/compiler.go
@@ -9,24 +9,68 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-    "strings"
+	"strings"
+    "sync"
+)
+
+// tokenizes and parses a single jackfile, optionally writing the xml files based on flags
+func tokenizeAndParse(jackFile string, wg *sync.WaitGroup) {
+    defer wg.Done()
+	file, err := os.Open(jackFile)
+	if err != nil {
+		log.Print(fmt.Sprint("Could not open file ", file))
+	}
+	reader := bufio.NewReader(file)
+	tokens := fe.Tokenize(reader)
+	if tokenXML {
+		tokenXmlFile, err := os.Create("build/" + strings.TrimSuffix(file.Name(), ".jack") + "_TOKENS.xml")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer tokenXmlFile.Close()
+		w := bufio.NewWriter(tokenXmlFile)
+		err = fe.TokenToXML(tokens, w)
+		if err != nil {
+			log.Print(fmt.Sprint("Could not write token XML for ", file.Name()))
+		}
+		w.Flush()
+	}
+	ast := fe.Parse(tokens)
+	if astXML {
+		astXmlFile, err := os.Create("build/" + strings.TrimSuffix(file.Name(), ".jack") + "_AST.xml")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer astXmlFile.Close()
+		w := bufio.NewWriter(astXmlFile)
+		err = fe.NodeToXML(ast, w)
+		if err != nil {
+			log.Print(fmt.Sprint("Could not write ast XML for ", file.Name()))
+		}
+		w.Flush()
+	}
+
+}
+
+var (
+	tokenXML, astXML bool
 )
 
 // the main function for the compiler, the entry point to the program
 func main() {
-	tokenXML := flag.Bool("tokens", false, "output tokens in xml")
-	astXML := flag.Bool("ast", false, "output ast in xml")
+	flag.BoolVar(&tokenXML, "tokens", false, "output tokens in xml")
+	flag.BoolVar(&astXML, "ast", false, "output ast in xml")
 	// xmlHeader := flag.Bool("xml-header", false, "output header when writing xml")
 	flag.Parse()
 
 	args := flag.Args()
-    if _, err := os.Stat("build"); !os.IsNotExist(err) {
-        os.RemoveAll("build")
-    }
-    err := os.Mkdir("build", os.ModePerm)
-    if err != nil {
-        log.Fatal("Could not create build/ folder")
-    }
+	if _, err := os.Stat("build"); !os.IsNotExist(err) {
+		os.RemoveAll("build")
+	}
+	err := os.Mkdir("build", os.ModePerm)
+	if err != nil {
+		log.Fatal("Could not create build/ folder")
+	}
 
 	var jackFiles []string
 	if len(args) == 0 {
@@ -48,39 +92,26 @@ func main() {
 	} else {
 		jackFiles = args
 	}
-	for _, jackFile := range jackFiles {
-		file, err := os.Open(jackFile)
-		if err != nil {
-			log.Print(fmt.Sprint("Could not open file ", file))
-		}
-		reader := bufio.NewReader(file)
-		tokens := fe.Tokenize(reader)
-		if *tokenXML {
-            tokenXmlFile, err := os.Create("build/" + strings.TrimSuffix(file.Name(), ".jack") + "_TOKENS.xml")
-            if err != nil {
-                log.Fatal(err)
-            }
-            defer tokenXmlFile.Close()
-            w := bufio.NewWriter(tokenXmlFile)
-			err = fe.TokenToXML(tokens, w)
-			if err != nil {
-				log.Print(fmt.Sprint("Could not write token XML for ", file.Name()))
-			}
-            w.Flush()
-		}
-		ast := fe.Parse(tokens)
-		if *astXML {
-            astXmlFile, err := os.Create("build/" + strings.TrimSuffix(file.Name(), ".jack") + "_AST.xml")
-            if err != nil {
-                log.Fatal(err)
-            }
-            defer astXmlFile.Close()
-            w := bufio.NewWriter(astXmlFile)
-			err = fe.NodeToXML(ast, w)
-			if err != nil {
-				log.Print(fmt.Sprint("Could not write ast XML for ", file.Name()))
-			}
-            w.Flush()
-		}
+
+    //  multi threading in GO is luvly
+    var wg sync.WaitGroup // primitive used for waiting on threads
+    for i := 0; i < len(jackFiles); i++ {
+        wg.Add(1) // tell it how many go routines we are waiting on
+        go tokenizeAndParse(jackFiles[i], &wg)
 	}
+    wg.Wait()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
