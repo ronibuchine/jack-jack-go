70dcbddde6d36c48856075f9414eef5ad40a6de0
ronibuchine <ronibuchine@gmail.com>
2022-06-01 17:28:33 +0300

    added write API implementation

--- lib/vmwriter/vmWriter.go
+++ lib/vmwriter/vmWriter.go
@@ -2,43 +2,62 @@ package vmwriter
 
 import (
 	"bufio"
+	"fmt"
 )
 
 type VMWriter struct {
-    w *bufio.Writer
-    labelCounter int
-    name string
+	w            *bufio.Writer
+	labelCounter int
+	name         string
 }
 
 func NewVMWriter(name string, w *bufio.Writer) *VMWriter {
-    return nil
+	return nil
 }
 
 func (vmw *VMWriter) WritePush(segment string, index int) {
+	vmw.w.WriteString(fmt.Sprintf("push %s %s\n", segment, index))
 }
 
 func (vmw *VMWriter) WritePop(segment string, index int) {
+	vmw.w.WriteString(fmt.Sprintf("pop %s %s\n", segment, index))
 }
 
 func (vmw *VMWriter) WriteArithmetic(command string) {
+	switch command {
+	case "+":
+		vmw.w.WriteString("add")
+	case "-":
+		vmw.w.WriteString("sub")
+	case "*":
+		vmw.w.WriteString("call Math.multiply 2")
+	case "/":
+		vmw.w.WriteString("call Math.divide 2")
+	}
 }
 
 // returns the unique label that was written
-func (vmw *VMWriter) WriteLabel() string {
-    return ""
+func (vmw *VMWriter) WriteLabel(label string) string {
+	vmw.w.WriteString(fmt.Sprintf("label %s\n", label))
+	return label
 }
 
-func (vmw *VMWriter) WriteGoto() {
+func (vmw *VMWriter) WriteGoto(label string) {
+	vmw.w.WriteString(fmt.Sprintf("goto %s\n", label))
 }
 
-func (vmw *VMWriter) WriteIf() {
+func (vmw *VMWriter) WriteIf(label string) {
+	vmw.w.WriteString(fmt.Sprintf("if-goto %s\n", label))
 }
 
 func (vmw *VMWriter) WriteCall(name string, nargs int) {
+	vmw.w.WriteString(fmt.Sprintf("call %s %s\n", name, nargs))
 }
 
 func (vmw *VMWriter) WriteFunction(name string, nargs int) {
+	vmw.w.WriteString(fmt.Sprintf("function %s %s\n", name, nargs))
 }
 
 func (vmw *VMWriter) WriteReturn() {
+	vmw.w.WriteString("return\n")
 }
